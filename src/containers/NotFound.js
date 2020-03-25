e50ca038b730fe0f3d3e44906756dbac423d9203
Jess Melanson <jmelanson38@massmutual.com>
2020-03-25 15:07:06 -0400

    Add 404 route

---
+++ src/containers/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './NotFound.css';
+
+const NotFound = () => (
+  <div className="NotFound">
+    <h3>Sorry, page not found!</h3>
+  </div>
+);
+
+export default NotFound;
