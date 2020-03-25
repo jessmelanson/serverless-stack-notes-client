5dcab1d871a4693a996950dc16404ee81f9f70fb
Jess Melanson <jmelanson38@massmutual.com>
2020-03-25 14:44:03 -0400

    Add homepage component; check in other new files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Link } from 'react-router-dom';
+import { Navbar } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import './App.css';
+import Routes from './Routes';
+
+const App = () => (
+  <div className="App container">
+    <Navbar fluid collapseOnSelect>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <Link to="/">Scratch</Link>
+        </Navbar.Brand>
+      </Navbar.Header>
+    </Navbar>
+    <Routes />
+  </div>
+);
 
 export default App;
